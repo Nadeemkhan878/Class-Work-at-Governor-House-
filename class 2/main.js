cb94863ea80e773e1f90d307e9286b282ab222c5
Nadeem khan <160847092+Nadeemkhan878@users.noreply.github.com>
2024-06-06 01:12:26 +0500

    Add files via upload

---
+++ class 2/main.js
@@ -0,0 +1,46 @@
+// let firstname ="sultan"
+//  let age:number = 18
+//  let isMarried:boolean=false
+//  console.log(firstname);
+//  console.log(age);
+//  console.log(isMarried);
+//  let gravity:number=9
+// gravity=10 
+// gravity=11
+// console.log(gravity);
+// let seb=1800
+// let bags =10
+// let total =seb + bags;
+// console.log(total) //181o
+//  let ek_ghar_me_log_hain=5
+//  let dusry_ghar_me_log_hain =10
+//  let  kitny_log_huwy=ek_ghar_me_log_hain+dusry_ghar_me_log_hain ;
+//  console.log(kitny_log_huwy) //15
+// let layer:number=5
+// let apple:number=5
+// let power:number=layer**2
+// console.log(power)//25
+// let totalapples:number=20;
+// let bag:number=4;
+// let eachbags:number=totalapples / bag
+// console.log(eachbags)//5
+// cost increment
+// let b =1
+// console.log(b++)
+// console.log(b++)
+// console.log(b++)
+// console.log(b++)
+// let a =30
+// console.log(a=a*15)
+// let a =4
+// console.log(a++)//4
+// console.log(a)//5
+// console.log(a)//5
+// console.log(a)//5
+//pre increment
+var b = 3;
+console.log(++b);
+console.log(++b);
+console.log(++b);
+console.log(b);
+console.log(b = b + 5);
