baaedd401c971c235d597c6a12ca072bf171ae70
Nadeem khan <160847092+Nadeemkhan878@users.noreply.github.com>
2024-06-08 17:28:28 +0500

    Add files via upload

---
+++ class 3/main.ts
@@ -0,0 +1,37 @@
+// let a:string="Ali"
+// let b:string="ali"// ali ka a cpital hai isi liye farq hai equel nhi hoga
+
+// console.log(a == b)  // false
+// console.log(a != b)  // true 
+// console.log(a > b)  // true 
+// console.log(a < b)  // false
+
+
+// let a:number = 10
+// let b:number =20
+// let c: number= 11
+// console.log(
+//  10<20 &&  10> 11
+//     // (a<b) && (a >c)
+// );//false is answer q k ek answer wrong hai a ki value hai 10 aur c ki value ha 11,
+
+
+// let a:number = 10
+// let b:number =20
+// let c: number=11
+// console.log(
+//   10<20 ||     10<11
+//     // (a < b) || (a < c)
+// );
+
+
+//  let a:string="14"
+//  let b:string="14"
+//  console.log(a!=b) //false
+
+// let a:string=`five`
+// let b:number=5
+// //@ts-ignore
+//          //  (a not Equal b),
+// console.log(a!=b)   // true answer aye ga qunky string ek akg veriable hai aur number ek alg variable hai.
+
