d9b0d42daa60e849e56b9c25d5096b1a52a28d31
Nadeem khan <160847092+Nadeemkhan878@users.noreply.github.com>
2024-06-20 16:15:37 +0500

    Add files via upload

---
+++ class 4/main.js
@@ -0,0 +1,83 @@
+// if and else statemants;
+// 01 mathed
+var alian_color = ["Red", "green", "yellow"];
+// if
+// 01 mathed
+// let age:number=17
+// if(age ==18){
+//     console.log(`you are aligable for id card`)
+// }
+// else{
+//     console.log(`you are not aligable for id card`)
+// } //is program me else print hoga q k umer kam hai;
+// let age:number=18
+// if(age ==18){
+//     console.log(`you are aligable for id card`)
+// }
+// else{
+//     console.log(`you are not aligable for id card`)
+// } //is program me if print hoga q k umer 18 k baraber hai;
+//02 mathed
+// let age:number=20
+// if(age >=18){
+//     console.log(`you are aligable for id card`)
+// }
+// else{
+//     console.log(`you are not aligable for id card`)
+// } //is program me if print hoga q k umer umer ziyada hai 18 sy ya 18 k braber hai;
+// let age:number=17.99
+// if(age >=18){
+//     console.log(`yoy are aligable for id card`)
+// }
+// else{
+//     console.log(`you are not aligable for id card`)
+// } //is program me else print hoga q k umer kam hai 18 sy aur ziyada bhi nhi hai 18 sy .18 sy kam hai;
+var email = "shoibali878@gmail.com";
+var password = "wrong password";
+var username = "shoib";
+if (username == "shoib" && password == "wrong password") {
+    console.log("logged in");
+}
+else {
+    console.log("wrong email and password please try again latter");
+} //is me if print hoga q k dono conditions some hain.
+// let email:string=`shoibali878@gmail.com`
+// let password:string=`wrong password`
+// if(email==`shoibali878@gmail.com` || password== `wrong password`){
+//     console.log(`logged in`)
+// }else{
+//     console.log(`wrong email and password please try again latter`)
+// } 
+//3rd method
+// let percantage=90
+// if(percantage >=90 && percantage <=100){
+//     console.log(`A+ grade`)
+// }
+//  else if(percantage >= 80 && percantage <=89.99){
+// console.log(`A grade`)
+//  }
+//  else if(percantage >= 70 && percantage <=79.99 ){
+//     console.log(`B grade`)
+//  }
+//  else if(percantage >=60 && percantage <=69.99){
+//     console.log(`C grade`)
+//  }
+//  else {
+//     console.log(`fail`)
+// }
+// let Mobile_packege=99
+// if(Mobile_packege >=100 && Mobile_packege <=149.99){
+//     console.log(`sms packege`)
+// }
+//  else if(Mobile_packege >= 150 && Mobile_packege <=249.99){
+// console.log(`3 days Hybrid packege`)
+//  }
+//  else if(Mobile_packege >= 250 && Mobile_packege <=349.99 ){
+//     console.log(`5 days sms packege`)
+//  }
+//  else if(Mobile_packege >=350 && Mobile_packege <=449.99){
+//     console.log(`weekely Internet packege`)
+//  }
+//  else {
+//     console.log(`Insifitiont balance for any one packege`)
+// }
